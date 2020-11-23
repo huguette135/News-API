9cd96cc14b088a492ee6ee57800550f1dc315acd
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-23 08:31:27 +0200

    add function of config

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 class Config:
@@ -10,28 +9,3 @@ class Config:
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
-
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
