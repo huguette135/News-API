96daf96aa49ab8c3abc85ce9d642cb48c800ec43
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-23 08:27:36 +0200

    add function of test

--- manage.py
+++ manage.py
@@ -12,5 +12,9 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-    if __name__ == '__main__':
+
+
+
+if __name__ == '__main__':
     manager.run()
+
